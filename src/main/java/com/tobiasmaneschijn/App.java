3e599b8b992263568ca0522a5135a99f300e858f
Tobias Maneschijn <tfrom222@gmail.com>
2021-01-17 20:43:03 +0100

    App starts a new Game

--- src/main/java/com/tobiasmaneschijn/App.java
+++ src/main/java/com/tobiasmaneschijn/App.java
@@ -1,5 +1,7 @@
 package com.tobiasmaneschijn;
 
+import com.tobiasmaneschijn.core.Game;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Game game = new Game();
+        game.startGame();
     }
 }
