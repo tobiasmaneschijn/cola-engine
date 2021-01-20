529205a18e2f9167ded19007d7540bf723e9ef8d
Tobias Maneschijn <tfrom222@gmail.com>
2021-01-20 19:13:38 +0100

    Added shaders to sprite and rendering

--- src/main/java/com/tobiasmaneschijn/core/Sprite.java
+++ src/main/java/com/tobiasmaneschijn/core/Sprite.java
@@ -1,11 +1,19 @@
 package com.tobiasmaneschijn.core;
 
+import org.joml.Math;
+import org.joml.Matrix4f;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL30;
 import org.lwjgl.opengl.GL45;
 
 import java.io.IOException;
 import java.nio.FloatBuffer;
+
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.glDrawArrays;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
 public class Sprite {
     /** The texture that stores the image for this sprite */
@@ -14,33 +22,51 @@ public class Sprite {
     /** The width in pixels of this sprite */
     private int width;
 
-    /** The height in pixels of this sprite */
+    Vector3f color;
+    /**
+     * The height in pixels of this sprite
+     */
     private int height;
-
-
-    /** id of the sprite vbo */
+    /**
+     * id of the sprite vbo
+     */
     private int vboId;
-
-    /** id of the sprite vao */
+    /**
+     * id of the sprite vao
+     */
     private int vaoId;
+    private Shader shader;
+
+
     /**
      * Create a new sprite from a specified image.
      *
      * @param window The window in which the sprite will be displayed
-     * @param ref A reference to the image on which this sprite should be based
+     * @param ref    A reference to the image on which this sprite should be based
      */
-    public Sprite(GameWindow window,String ref) {
+    public Sprite(GameWindow window, String ref) {
+        color = new Vector3f(.5f,0,0);
+
         try {
             texture = window.getTextureLoader().getTexture(ref);
 
             width = texture.getImageWidth();
             height = texture.getImageHeight();
+
+            shader = new Shader("sprite", "sprite");
         } catch (IOException e) {
             // a tad abrupt, but our purposes if you can't find a
             // sprite's image you might as well give up.
             System.err.println("Unable to load texture: "+ref);
             System.exit(0);
         }
+
+        Matrix4f projection = new Matrix4f()
+                .ortho(0,window.getWidth(),window.getHeight(),0, -1,1);
+        shader.setInteger("image", 0, true);
+        shader.setMatrix4("projection", projection, true);
+
+
     }
 
     /**
@@ -61,53 +87,73 @@ public class Sprite {
         return texture.getImageHeight();
     }
 
-    /**
-     * Draw the sprite at the specified location
-     *
-     * @param x The x location at which to draw this sprite
-     * @param y The y location at which to draw this sprite
-     */
-    public void draw(int x, int y) {
 
-        float[] vertices = {
-                // Left bottom triangle
-                -0.5f, 0.5f, 0f,
-                -0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                // Right top triangle
-                0.5f, -0.5f, 0f,
-                0.5f, 0.5f, 0f,
-                -0.5f, 0.5f, 0f
+    public void initRenderData() {
+
+        float vertices[] = {
+                // pos      // tex
+                0.0f, 1.0f, 0.0f, 1.0f,
+                1.0f, 0.0f, 1.0f, 0.0f,
+                0.0f, 0.0f, 0.0f, 0.0f,
+
+                0.0f, 1.0f, 0.0f, 1.0f,
+                1.0f, 1.0f, 1.0f, 1.0f,
+                1.0f, 0.0f, 1.0f, 0.0f
         };
 
-        // Sending data to OpenGL requires the usage of (flipped) byte buffers
+        vaoId = glGenVertexArrays();
+        vboId = glGenBuffers();
+
         FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
         verticesBuffer.put(vertices);
         verticesBuffer.flip();
 
-        int vertexCount = 6;
-
-        // Create a new Vertex Array Object in memory and select it (bind)
-// A VAO can have up to 16 attributes (VBO's) assigned to it by default
-        vaoId = GL30.glGenVertexArrays();
-        GL30.glBindVertexArray(vaoId);
-        GL45.glEnableVertexAttribArray(0);
-
-// Create a new Vertex Buffer Object in memory and select it (bind)
-// A VBO is a collection of Vectors which in this case resemble the location of each vertex.
-        vboId = GL45.glGenBuffers();
         GL45.glBindBuffer(GL45.GL_ARRAY_BUFFER, vboId);
         GL45.glBufferData(GL45.GL_ARRAY_BUFFER, verticesBuffer, GL45.GL_STATIC_DRAW);
-// Put the VBO in the attributes list at index 0
-        GL45.glVertexAttribPointer(0, 3, GL45.GL_FLOAT, false, 0, 0);
 
-        // Draw the vertices
-        GL45.glDrawArrays(GL45.GL_TRIANGLES, 0, vertexCount);
+        GL45.glBindVertexArray(vaoId);
+        GL45.glEnableVertexAttribArray(0);
+        GL45.glVertexAttribPointer(0, 4, GL45.GL_FLOAT, false, 0, 0);
 
-// Deselect (bind to 0) the VBO
         GL45.glBindBuffer(GL45.GL_ARRAY_BUFFER, 0);
+        GL45.glBindVertexArray(0);
+    }
 
-// Deselect (bind to 0) the VAO
+
+    public Vector3f getColor() {
+        return color;
+    }
+
+    public void setColor(Vector3f color) {
+        this.color = color;
+    }
+
+    /**
+     * Draw the sprite at the specified location
+     *
+     */
+    public void draw(Vector2f position, Vector2f size, float rotate) {
+
+        // prepare transformations
+        shader.use();
+        Matrix4f model = new Matrix4f();
+        model = model.translate(new Vector3f());
+        model = model.translate(new Vector3f(position, 0.0f));
+
+        model = model.translate(new Vector3f(0.5f * size.x, 0.5f * size.y, 0.0f));
+        model = model.rotate(Math.toRadians(rotate), new Vector3f(0.0f, 0.0f, 1.0f));
+        model = model.translate(new Vector3f(-0.5f * size.x, -0.5f * size.y, 0.0f));
+
+        model = model.scale(new Vector3f(size, 1.0f));
+
+        shader.setMatrix4("model", model, true);
+        shader.setVector3f("spriteColor", color, true);
+
+        glActiveTexture(GL_TEXTURE0);
+        texture.bind();
+
+        GL45.glBindVertexArray(vaoId);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
         GL45.glBindVertexArray(0);
 
     }
@@ -120,4 +166,8 @@ public class Sprite {
         return vaoId;
     }
 
+    public void destroy() {
+        GL45.glDeleteBuffers(vboId);
+        GL45.glDeleteVertexArrays(vaoId);
+    }
 }
